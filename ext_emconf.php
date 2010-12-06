c711a15d1d7473cf8229c1ea0507585f2a55d3c4
bladerunner <bladerunner@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-12-06 20:37:52 +0000

    version 2.0.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/joliprint/trunk@40898 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "joliprint".
 #
-# Auto generated 14-11-2010 22:22
+# Auto generated 06-12-2010 21:37
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '2.0.2',
+	'version' => '2.0.3',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"67d0";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"b596";s:17:"ext_localconf.php";s:4:"af8d";s:14:"ext_tables.php";s:4:"ff02";s:24:"ext_typoscript_setup.txt";s:4:"e492";s:13:"locallang.xml";s:4:"d3ca";s:16:"locallang_db.xml";s:4:"08f3";s:14:"doc/manual.sxw";s:4:"ab71";s:19:"doc/wizard_form.dat";s:4:"a1e2";s:20:"doc/wizard_form.html";s:4:"a97a";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_joliprint_pi1.php";s:4:"ddbf";s:38:"pi1/class.tx_joliprint_pi1_wizicon.php";s:4:"5944";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"0987";s:14:"res/config.xml";s:4:"cd3f";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"67d0";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"b596";s:17:"ext_localconf.php";s:4:"af8d";s:14:"ext_tables.php";s:4:"ff02";s:24:"ext_typoscript_setup.txt";s:4:"e492";s:13:"locallang.xml";s:4:"d3ca";s:16:"locallang_db.xml";s:4:"08f3";s:14:"doc/manual.sxw";s:4:"ab71";s:19:"doc/wizard_form.dat";s:4:"a1e2";s:20:"doc/wizard_form.html";s:4:"a97a";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_joliprint_pi1.php";s:4:"87f3";s:38:"pi1/class.tx_joliprint_pi1_wizicon.php";s:4:"5944";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"0987";s:14:"res/config.xml";s:4:"cd3f";}',
 	'suggests' => array(
 	),
 );
